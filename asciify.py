2fd00ef4e967ff52ca1e0819eebd593dba394242
Samuele Mattiuzzo <sam@potatolondon.com>
2013-06-27 13:36:50 +0100

    Image to Ascii to Image again

--- asciify.py
+++ asciify.py
@@ -8,9 +8,14 @@ except ImportError:
 import os
 import random
 import urllib
-import cStringIO
+import StringIO
 
 from bisect import bisect
+
+MAX_SIZE = 350, 350
+COLOR_WHITE = (255, 255, 255)
+FILL = 'black'
+OUTLINE = 'black'
 
 # greyscale.. the following strings represent
 # 7 tonal ranges, from lighter to darker.
@@ -37,7 +42,15 @@ class UnsupportedFormatException(Exception):
 
 
 def _url_to_img(url):
-    return cStringIO.StringIO(urllib.urlopen(url).read())
+
+    # awesome variable names.
+    _la_image_from_la_url = urllib.urlopen(url).read()
+    _la_buff_for_la_image = StringIO.StringIO()
+
+    _la_buff_for_la_image.write(_la_image_from_la_url)
+    _la_buff_for_la_image.seek(0)
+
+    return _la_buff_for_la_image
 
 
 def to_ascii(url):
@@ -49,37 +62,47 @@ def to_ascii(url):
 
     try:
         im = Image.open(_la_image)
-    except:
+    except Exception, e:
+        print e
         raise UnsupportedFormatException("Your file must be a valid .png image")
 
-    im = im.resize((160, 75), Image.BILINEAR)
+    im = im.resize(MAX_SIZE, Image.BILINEAR)
     im = im.convert("L")  # convert to mono
+
+    w, h = im.size
 
     # now, work our way over the pixels
     # build up str
     out = ""
-    for y in range(0, im.size[1]):
-        for x in range(0, im.size[0]):
+    for y in range(0, h):
+        for x in range(0, w):
             lum = 255-im.getpixel((x, y))
             row = bisect(ZONEBOUNDS, lum)
             possibles = GREYSCALE[row]
             out = out + possibles[random.randint(0, len(possibles)-1)]
         out = out+"\n"
-    return out
+    return out, w, h
 
 
 def to_image(url):
 
-    img = Image.new('L', (500, 500), 255)
-    draw = ImageDraw.Draw(img)
-    img_content = to_ascii(url)
-    font = ImageFont.truetype(os.getcwd() + '/font/anon.ttf', 10)
+    img_content, w, h = to_ascii(url)
 
-    draw.text((0, 0), img_content, font=font, fill=100)
+    img = Image.new('RGB', (w, h), COLOR_WHITE)
+    draw = ImageDraw.Draw(img)
+    font = ImageFont.truetype(os.getcwd() + '/font/anon.ttf', 2)
+
+    cur_line = 0
+    for line in img_content.split('\n'):
+        draw.text((0, cur_line), line, font=font, fill=FILL)
+        cur_line += 1
     del draw
 
+    img.thumbnail(MAX_SIZE, Image.ANTIALIAS)
     img.save('output.png')
 
 
 if __name__ == '__main__':
-    print to_image('http://upload.wikimedia.org/wikipedia/commons/8/87/Avatar_poe84it.png')
+    url = 'http://images.wikia.com/donkeykong/images/archive/1/17/20110926232526!BananaDKCR.png'
+    #url = 'http://upload.wikimedia.org/wikipedia/commons/8/87/Avatar_poe84it.png'
+    to_image(url)
